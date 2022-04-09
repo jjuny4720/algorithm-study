a975a44db5ae20ec0d2c914da2a28448cc9030d6
jjuny4720 <jjuny4720@naver.com>
2022-04-09 13:36:28 +0900

    add js

---
+++ joon/Greedy/ex3greedy.js
@@ -0,0 +1,22 @@
+function solution(n, lost, reserve) {
+    
+  const clothes = Array(n).fill(1);
+  
+  lost.map(function(lost){
+      clothes[lost-1] = 0});
+  
+  reserve.map(function(reserve){
+      clothes[reserve-1] += 1});
+  
+  for(let i=0; i<n; i++){
+      if(clothes[i] == 0 && clothes[i-1] === 2){
+          clothes[i] = 1;
+          clothes[i-1] = 1;
+      }
+      else if(clothes[i] === 0 && clothes[i+1] === 2){
+          clothes[i] = 1;
+          clothes[i+1] = 1;
+      }
+  }
+  return clothes.filter(c => c > 0).length;
+}
