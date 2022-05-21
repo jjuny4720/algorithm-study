91b0c1f2ed67d44dd5869621f924c25d962d358a
ksg0605 <ksg0605@gmail.com>
2022-05-21 20:35:34 +0900

    add summary of DP and solved problem.

--- sunkyu/binary-search/BOJ7795.py
+++ sunkyu/binary-search/BOJ7795.py
@@ -4,11 +4,10 @@ import bisect
 input = sys.stdin.readline
 t = int(input())
 for _ in range(t):
-  count = 0
-  n, m = input().split()
-  a = sorted(list(map(int, input().split())))
-  b = sorted(list(map(int, input().split())))
-  for item in a:
-    count += bisect.bisect_left(b, item)
-  print(count)
-  
+    count = 0
+    n, m = input().split()
+    a = sorted(list(map(int, input().split())))
+    b = sorted(list(map(int, input().split())))
+    for item in a:
+        count += bisect.bisect_left(b, item)
+    print(count)
